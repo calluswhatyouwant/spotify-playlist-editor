2c04e75f4a602f09584eb18829aeb5927f64dc7c
JRobsonJr <jrobsonjr16@gmail.com>
2019-04-21 13:41:22 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+    <div className="app">
+        <h1>Spotify Playlist Editor</h1>
+        <p>Insert content here!</p>
+    </div>
+);
+
+export default App;
