822e96943b50f7f1eb09e638d0ebe22c8de57e12
JRobsonJr <jrobsonjr16@gmail.com>
2019-04-22 19:11:58 -0300

    Create Home component

---
+++ src/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => (
+    <div className="home">
+        <div className="text-center">
+            <h1 className="display-1">
+                Spotify Playlist Editor
+            </h1>
+            <h4>
+                Sort your playlists any way you want.
+            </h4>
+            <a className="btn btn-lg btn-outline-light my-2" href={buildAuthUrl()}>
+                Connect to Spotify
+            </a>
+        </div>
+    </div>
+);
+
+const buildAuthUrl = () => {
+    const params = {
+        client_id: 'SPOTIFY_CLIENT_ID',
+        response_type: 'token',
+        redirect_uri: 'http://localhost:3000/auth',
+        scope: 'playlist-read-private playlist-read-collaborative'
+    };
+    return `https://accounts.spotify.com/authorize?${encodeQueryParams(params)}`;
+}
+
+const encodeQueryParams = (params) => Object.keys(params)
+    .map(param => `${encodeURIComponent(param)}=${encodeURIComponent(params[param])}`)
+    .join("&");
+
+export default Home;
