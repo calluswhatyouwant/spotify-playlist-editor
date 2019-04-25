4818eb1193c4428b0d646dc62ba9e6008708869c
JRobsonJr <jrobsonjr16@gmail.com>
2019-04-25 18:50:42 -0300

    Add caution note to Home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -9,6 +9,11 @@ const Home = () => (
         <div className="text-center">
             <h1 className="display-1">Spotify Playlist Editor</h1>
             <h4>Sort your playlists any way you want.</h4>
+            <h5>
+                <i class="fas fa-exclamation-triangle" /> Note this is still a
+                work in progress, so use it with caution.{' '}
+                <i class="fas fa-exclamation-triangle" />
+            </h5>
             <a
                 className="btn btn-lg btn-outline-light my-2"
                 href={buildAuthUrl()}
@@ -21,7 +26,7 @@ const Home = () => (
 
 const buildAuthUrl = () => {
     const params = {
-        client_id: 'SPOTIFY_CLIENT_ID',
+        client_id: '77edb9a2465445fc8b366da5105e7add',
         response_type: 'token',
         redirect_uri: 'http://localhost:3000/auth',
         scope:
