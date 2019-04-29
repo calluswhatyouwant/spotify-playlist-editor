84e4ef0092f66e200de8096de05a2c54f2746bd4
JRobsonJr <jrobsonjr16@gmail.com>
2019-04-28 22:24:36 -0300

    Updates

--- precache-manifest.d50694061777808fe3b63391a6bab76b.js
+++ precache-manifest.d50694061777808fe3b63391a6bab76b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/spotify-playlist-editor/static/js/runtime~main.1a7e21be.js"
   },
   {
-    "revision": "9c0f40521a76f87d7107",
-    "url": "/spotify-playlist-editor/static/js/main.8a1f0cef.chunk.js"
+    "revision": "1864e076a5e42e322c3c",
+    "url": "/spotify-playlist-editor/static/js/main.e91a1f65.chunk.js"
   },
   {
     "revision": "b84ab63cc95a9ee8e821",
     "url": "/spotify-playlist-editor/static/js/2.cbb3faac.chunk.js"
   },
   {
-    "revision": "9c0f40521a76f87d7107",
+    "revision": "1864e076a5e42e322c3c",
     "url": "/spotify-playlist-editor/static/css/main.fb4b5d14.chunk.css"
   },
   {
-    "revision": "eaaf62ba7f69eb2ada159b688c6b7408",
+    "revision": "72a92752041aecf5a8d1e7f3395fae49",
     "url": "/spotify-playlist-editor/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/spotify-playlist-editor/precache-manifest.b03f5630bc52fad07667339c19e5c329.js"
+  "/spotify-playlist-editor/precache-manifest.d50694061777808fe3b63391a6bab76b.js"
 );
 
 workbox.clientsClaim();
